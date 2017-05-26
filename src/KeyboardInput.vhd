76eb5d6d8eaa485b8e2215b958b7e5acb01cbb25
bill125 <bill125@gmail.com>
2017-05-27 02:01:01 +0800

    update main source

--- src/KeyboardInput.vhd
+++ src/KeyboardInput.vhd
@@ -3,14 +3,16 @@ use ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 
-entity input_keyboard is
+entity KeyboardInput is
 port(
 	datain,clkin,fclk,rst_in: in std_logic;
+	key_out: out std_logic_vector(7 downto 0);
 	seg0, seg1:out std_logic_vector(6 downto 0)
+	clk_out: out std_logic;
 );
-end input_keyboard;
+end KeyboardInput;
 
-architecture behave of input_keyboard is
+architecture behave of KeyboardInput is
 component Keyboard is
 port (
 	datain, clkin : in std_logic ; -- PS2 clk and data
@@ -31,11 +33,12 @@ signal scancode : std_logic_vector(7 downto 0);
 signal rst : std_logic;
 signal clk_f: std_logic;
 begin
-rst<=not rst_in;
+	rst<=not rst_in;
 
-u0: Keyboard port map(datain,clkin,fclk,rst,scancode);
-u1: seg7 port map(scancode(3 downto 0),seg0);
-u2: seg7 port map(scancode(7 downto 4),seg1);
-
+	u0: Keyboard port map(datain,clkin,fclk,rst,scancode);
+	key_out <= scancode;
+	u1: seg7 port map(scancode(3 downto 0),seg0);
+	u2: seg7 port map(scancode(7 downto 4),seg1);
+	
 end behave;
 
---
+++ src/Velocity.vhd
@@ -0,0 +1,36 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Velocity is
+	port (
+		i_clk : in std_logic;
+		i_key : in std_logic_vector(7 downto 0);
+		o_vel : out array (0 to 5) of integer range 0 to 127
+	);
+end Velocity;
+
+architecture Velocity_bhv of Velocity is
+signal vel : integer range 0 to 127 := 64;
+begin
+	o_vel <= vel;
+	monitor : process (i_clk)
+	begin
+		if (i_clk'event and i_clk = '1') then begin
+			if i_key = "01110010" then -- '8 on pad' : 72
+				if vel <= 126 then
+					vel <= vel + 1;
+				else
+					vel <= vel;
+				end;
+			elsif i_key = "01110101" then -- '2 on pad' : 75
+				if vel >= 1 then
+					vel <= vel - 1;
+				else
+					vel <= vel;
+				end;
+			else
+				vel <= vel;
+			end;
+		end;
+	end;
+end Velocity_bhv;
