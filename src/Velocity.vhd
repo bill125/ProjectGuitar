20ec5596f5ed490264b1d332ed8c054ccefc7b3e
lazycal <lazycal12@gmail.com>
2017-05-27 00:03:31 +0800

    add velocity.vhd

---
+++ src/Velocity.vhd
@@ -0,0 +1,11 @@
+entity Velocity is
+  port (
+	seg0, seg1 : in std_logic_vector(6 downto 0);
+	vel : array (0 to 5) of integer
+    );
+end entity Velocity;
+
+architecture beh of Velocity is
+begin
+  
+end
