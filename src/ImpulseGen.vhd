36946f7df57b47e9dc17cf4fb7ad5f6819521c1e
lazycal <lazycal12@gmail.com>
2017-05-27 20:08:11 +0800

    update ImpulseGen.vhd

--- src/ImpulseGen.vhd
+++ src/ImpulseGen.vhd
@@ -1,30 +1,36 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
 entity ImpulseGen is
 	 generic 
 	 ( 
-	 pausePeriod: integer := 10
+	 pausePeriod: integer range 0 to 63 := 10
 	 );
 	port (
-		start : inout std_logic;
+		start : in std_logic;
 		hclk : in std_logic;
-		clk : inout std_logic
+		clk_out : out std_logic := '0'
 	);
 end entity ImpulseGen;
 	
 architecture beh of ImpulseGen is
+    signal cnt: integer range 0 to 63 := pausePeriod + 1;
 begin
   process (hclk) is
-    variable cnt: integer := 0;
+	 variable pre_start: std_logic := start;
   begin
     if rising_edge(hclk) then
-      if start = '1' then
-        start <= '0';
-        cnt := 0;
+      if start /= pre_start then
+        cnt <= 0;
+		  clk_out <= '0';
       elsif cnt <= pausePeriod then
-        cnt := cnt + 1;
-        if cnt >= pausePeriod then
-          clk <= not clk;
+        cnt <= cnt + 1;
+        if cnt = pausePeriod - 1 then
+          clk_out <= '1';
+		  elsif cnt >= pausePeriod then
+			 clk_out <= '0';
         end if;        
-      end if;      
+      end if;  
+		pre_start := start;
+	 end if;
   end process;
-  
 end architecture beh;
